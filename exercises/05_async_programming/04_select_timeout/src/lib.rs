d21bd7ccd343c69788e6b4e70e65b431a0d48378
AzureZee <azureze@qq.com>
2026-03-11 21:16:52 +0800

    05.04

--- exercises/05_async_programming/04_select_timeout/src/lib.rs
+++ exercises/05_async_programming/04_select_timeout/src/lib.rs
@@ -19,9 +19,9 @@ pub async fn with_timeout<F, T>(future: F, timeout_ms: u64) -> Option<T>
 where
     F: Future<Output = T>,
 {
-    // TODO: Use tokio::select! to race between future and sleep
-    // Or use tokio::time::timeout
-    todo!()
+    tokio::time::timeout(Duration::from_millis(timeout_ms), future)
+        .await
+        .ok()
 }
 
 /// Race two async tasks, return the result of whichever finishes first.
@@ -34,7 +34,10 @@ where
 {
     // TODO: Use tokio::select! to wait for f1 and f2
     // Return the result of whichever completes first
-    todo!()
+    tokio::select! {
+        result = f1 => result,
+        result = f2 => result,
+    }
 }
 
 #[cfg(test)]
